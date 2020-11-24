bc003968c7d6d225c50fc333b2545806e4042591
Tony <syntony666@outlook.com>
2020-11-24 22:10:23 +0800

    modify url and make serializer more raw

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,6 +3,5 @@ from user_management import views
 
 urlpatterns = [
     path('', views.user_list),
-    # path('?<str:key>=<str:value>', views.user_query),
-    path('id/<str:pk>/', views.user_detail),
+    path('<str:pk>', views.user_detail),
 ]
